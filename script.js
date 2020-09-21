128d45f11aba82d8f9d127533cc4bb2e6a042870
hibtmimi <hibatamimi24@gmail.com>
2020-09-21 13:15:48 +0300

    finel

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 /*
 - create an array
 - function to build quiz
@@ -9,53 +6,52 @@
 */
 
 // Array of questions
-var questionArray = [
-    {
-      question: "JQuery is a:",
-      answers: {
-        a: "JavaScript Library",
-        b: "JavaScript Language",
-        c: "JavaScript Method"
-      },
-      correctAnswer: "a"
+var questionArray = [{
+    question: "JQuery is a:",
+    answers: {
+      a: "JavaScript Library",
+      b: "JavaScript Language",
+      c: "JavaScript Method"
     },
-    {
-      question: "Which sign does jQuery use as a shortcut for jQuery:",
-      answers: {
-        a: "the % sign",
-        b: "the ? sign",
-        c: "the $ sign"
-      },
-      correctAnswer: "c"
+    correctAnswer: "a"
+  },
+  {
+    question: "Which sign does jQuery use as a shortcut for jQuery:",
+    answers: {
+      a: "the % sign",
+      b: "the ? sign",
+      c: "the $ sign"
     },
-     {
-      question: "Which jQuery method is used to hide selected elements?",
-      answers: {
-        a: "hidden()",
-        b: "hide()",
-        c: "display(none)"
-      },
-      correctAnswer: "b"
+    correctAnswer: "c"
+  },
+  {
+    question: "Which jQuery method is used to hide selected elements?",
+    answers: {
+      a: "hidden()",
+      b: "hide()",
+      c: "display(none)"
     },
-     {
-      question: "What scripting language is jQuery written in?",
-      answers: {
-        a: "VBScript",
-        b: "JavaScript",
-        c: "Java"
-      },
-      correctAnswer: "b"
+    correctAnswer: "b"
+  },
+  {
+    question: "What scripting language is jQuery written in?",
+    answers: {
+      a: "VBScript",
+      b: "JavaScript",
+      c: "Java"
     },
-     {
-      question: "Which of these is NOT a valid comment in JavaScript?",
-      answers: {
-        a: "// This is a comment",
-        b: "/* This is a comment */",
-        c: "/* This is a comment"
-      },
-      correctAnswer: "c"
-    }
-  ];
+    correctAnswer: "b"
+  },
+  {
+    question: "Which of these is NOT a valid comment in JavaScript?",
+    answers: {
+      a: "// This is a comment",
+      b: "/* This is a comment */",
+      c: "/* This is a comment"
+    },
+    correctAnswer: "c"
+  }
+];
 
 
 var quizBody = document.getElementById('quiz');
@@ -63,10 +59,9 @@ var resultsContainer = document.getElementById('results');
 var submit = document.getElementById('submit');
 
 
-
 /* Function to build question  */
 
-function quizFunc(){
+function quizFunc() {
   // variable to store the HTML output
   var output = [];
 
@@ -76,11 +71,11 @@ function quizFunc(){
       var answers = [];
 
       // and for each available answer...
-      for(letter in currentQuestion.answers){
+      for (letter in currentQuestion.answers) {
 
         // ...add an HTML radio button
         answers.push(
-          `<label>
+          `<label id="label">
             <input type="radio" name="question${number}" value="${letter}">
             ${letter}: ${currentQuestion.answers[letter]}
           </label>`
@@ -101,17 +96,17 @@ function quizFunc(){
 
 /* Function to show result  */
 
-function show(){
+function show() {
 
   // gather answer  from our quiz
 
-   // The querySelector() method only returns the first element that matches the specified selectors. To return all the matches, use the querySelectorAll() method instead.
+  // The querySelector() method only returns the first element that matches the specified selectors. To return all the matches, use the querySelectorAll() method instead.
   var answerContainers = quizBody.querySelectorAll('.answers');
 
   var numCorrect = 0;
 
   // for each question
- questionArray.forEach( (currentQuestion, number) => {
+  questionArray.forEach((currentQuestion, number) => {
 
     // find selected answer
     const answerContainer = answerContainers[number];
@@ -119,14 +114,14 @@ function show(){
     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
     // if answer is correct
-    if(userAnswer === currentQuestion.correctAnswer){
+    if (userAnswer === currentQuestion.correctAnswer) {
       // add to the number of correct answers
       numCorrect++;
-     }
+    }
   });
 
   // show number of correct answers out of total
-   $("#results").text(`Your result  : ${numCorrect}  out of  ${questionArray.length}`)
+  $("#results").text(`Your result  : ${numCorrect}  out of  ${questionArray.length}`)
 }
 
 // display quiz 
